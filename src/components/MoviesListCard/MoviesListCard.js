d1f86b305cd51e884f9982e0298a0a0855c94673
gulchizxvr <kyrylo.hulchenko@gmail.com>
2022-10-18 15:34:12 +0300

    17.10.2022 20:17 add genre

--- src/components/MoviesListCard/MoviesListCard.js
+++ src/components/MoviesListCard/MoviesListCard.js
@@ -3,19 +3,20 @@ import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
 
 import css from './MovieListCard.module.css'
+import {CBadge, CButton} from "@coreui/react";
 
 
 
 const MoviesListCard = ({movie}) => {
 
 
-    const {title, overview, poster_path, genre_ids} = movie
+    const {title, overview, poster_path, genre_ids,id} = movie
 
 
     // ***
     let navigate = useNavigate()
     const somesth = () => {
-        navigate("/ppp")
+        navigate(`/${title}`)
     }
     // ****
 
@@ -25,25 +26,11 @@ const MoviesListCard = ({movie}) => {
     const array = t.genres ? t.genres : []
     const genreOfMovie= []
 
-
-    // for (const genre of genre_ids) {
-    //     for (const gen of array) {
-    //         if (genre === gen.id){
-    //             genreOfMovie.push(gen.name);
-    //         }
-    //     }
-    // }
-    // console.log(genreOfMovie);
-
-
      array.map((item) => {
         if (genre_ids.includes(item.id)) {
            genreOfMovie.push(item.name)
        }
     })
-
-    genreOfMovie.length>0 ? console.log(genreOfMovie) : null
-
 
     // ***
     return (
@@ -52,13 +39,6 @@ const MoviesListCard = ({movie}) => {
             <img src={"https://image.tmdb.org/t/p/w300" + poster_path}/>
             <p>{overview}</p>
             <h4>{JSON.stringify(genreOfMovie)}</h4>
-
-
-            <div>
-
-
-            </div>
-
         </div>
     );
 };
