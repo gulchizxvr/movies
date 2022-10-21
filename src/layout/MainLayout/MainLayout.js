02a4bf5fd0da977627ffd623f95675a64d94f530
gulchizxvr <kyrylo.hulchenko@gmail.com>
2022-10-21 16:16:40 +0300

    add buttons - done

--- src/layout/MainLayout/MainLayout.js
+++ src/layout/MainLayout/MainLayout.js
@@ -5,9 +5,11 @@ import React from 'react';
 import {Header} from "../../components";
 import {Outlet} from "react-router-dom";
 
+import css from "./MainLayout.module.css"
+
 const MainLayout = () => {
     return (
-        <div>
+        <div className={css.all}>
            <Header/>
             <Outlet/>
         </div>
