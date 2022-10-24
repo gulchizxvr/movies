f2a0d84f71dd46b7a788b787cd337325a021870d
gulchizxvr <kyrylo.hulchenko@gmail.com>
2022-10-24 13:36:02 +0300

    add some css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,7 @@ import css from "./Button.module.css"
 const Button = ({genre,setGenre}) => {
 
     return (
-        <div className={css.onebutton}>
+        <div className={css.oneButton}>
            <button onClick={()=> setGenre(genre.id)}>{genre.name}</button>
         </div>
     );
